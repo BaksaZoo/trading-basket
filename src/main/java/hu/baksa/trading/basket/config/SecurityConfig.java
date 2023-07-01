897e65c2feccb0405511c962b4a73695f47f630c
BaksaZoo <baksazoo@hotmail.com>
2023-07-01 23:26:26 +0200

    feat: add authentication

--- src/main/java/hu/baksa/trading/basket/config/SecurityConfig.java
+++ src/main/java/hu/baksa/trading/basket/config/SecurityConfig.java
@@ -1,13 +1,16 @@
 package hu.baksa.trading.basket.config;
 
+import hu.baksa.trading.basket.security.JwtAuthConverter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
 @Slf4j
 @Configuration
@@ -15,13 +18,24 @@ import org.springframework.security.web.SecurityFilterChain;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final JwtAuthConverter jwtAuthConverter;
+
+    @Bean
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        // TODO: 2023. 06. 30. configure security properly
         http.authorizeHttpRequests(
-                        auth -> auth.anyRequest().permitAll()
-                )
-                .csrf(AbstractHttpConfigurer::disable);
+                auth -> auth.requestMatchers("/api/**")
+                        .hasRole("USER")
+                        .anyRequest()
+                        .permitAll()
+        );
+        http.oauth2ResourceServer(resourceServerConfigurer ->
+                resourceServerConfigurer.jwt(jwtConfigurer -> jwtConfigurer.jwtAuthenticationConverter(jwtAuthConverter))
+        );
         return http.build();
     }
 }
